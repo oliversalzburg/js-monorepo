1d91e45afb0b4d9a880fd2a20a1441112eabd604
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-05-01 17:04:03 +0200

    fix: Remove port forwardings

--- packages/documentation/.scripts/publish.sh
+++ packages/documentation/.scripts/publish.sh
@@ -5,4 +5,4 @@ basedir=$(dirname "$self")
 
 cd ${basedir}/../../..
 
-docker run --rm -it -p 8000:8000 -v ${PWD}:/docs ghcr.io/oliversalzburg/mkdocs-material-ex:main gh-deploy --config-file packages/documentation/mkdocs.yml --force
+docker run --rm -it -v ${PWD}:/docs ghcr.io/oliversalzburg/mkdocs-material-ex:main gh-deploy --config-file packages/documentation/mkdocs.yml --force
