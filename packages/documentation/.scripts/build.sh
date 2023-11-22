8a307af3535020d6a7ed434304ff90bc87db0c47
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-11-22 18:18:35 +0100

    fix: Use upstream mkdocs-material

--- packages/documentation/.scripts/build.sh
+++ packages/documentation/.scripts/build.sh
@@ -5,4 +5,4 @@ basedir=$(dirname "$self")
 
 cd ${basedir}/../../..
 
-docker run --rm -v ${PWD}:/docs ghcr.io/oliversalzburg/mkdocs-material-ex:main build --config-file packages/documentation/mkdocs.yml --site-dir=public
+podman run --rm -v ${PWD}:/docs docker.io/squidfunk/mkdocs-material build --config-file packages/documentation/mkdocs.yml --site-dir=public
--- packages/documentation/.scripts/publish.sh
+++ packages/documentation/.scripts/publish.sh
@@ -5,4 +5,4 @@ basedir=$(dirname "$self")
 
 cd ${basedir}/../../..
 
-docker run --rm -v ${PWD}:/docs ghcr.io/oliversalzburg/mkdocs-material-ex:main gh-deploy --config-file packages/documentation/mkdocs.yml --force
+podman run --rm -v ${PWD}:/docs docker.io/squidfunk/mkdocs-material gh-deploy --config-file packages/documentation/mkdocs.yml --force
