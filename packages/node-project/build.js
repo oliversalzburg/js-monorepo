fb63b0c7cfef90548d5264a079bc815cd0dcbbc6
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-09-15 19:34:10 +0200

    fix: Import

--- packages/node-project/build.js
+++ packages/node-project/build.js
@@ -1,4 +1,4 @@
-import { redirectErrorsToConsole } from "@oliversalzburg/js-utils";
+import { redirectErrorsToConsole } from "@oliversalzburg/js-utils/error/console.js";
 import esbuild from "esbuild";
 
 esbuild
